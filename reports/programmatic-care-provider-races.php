e8a0701771035ffa0c881751a18d1e50951f05a5
Wayne Glassbrook <wayne@ciesadesign.com>
2024-11-14 16:53:59 -0500

    Updated reports as templates

--- reports/programmatic-care-provider-races.php
+++ reports/programmatic-care-provider-races.php
@@ -5,7 +5,6 @@
  *  Report Type: Programmatic
  *  Report Name: Care Provider Races
  *  Scope: Consultant, Program, Statewide
- *  Requirement: gcharts
  *  Description: Pie chart displaying number/percentage of provider races
  *  Version: 1.0
  * 
@@ -22,72 +21,19 @@ if($report_scope == 'Consultant') {
 }else{
   $report_scope_slug = 'statewide';
 }
+
 // Start output buffering
 ob_start();
 ?>
 
-<style>
-#chartdiv {
-  display: none;
-  width: 100%;
-  height: 500px;
-}
-</style>
-
-<!-- HTML -->
 <h2>Programmatic - Care Provider Races - <?php echo esc_html($report_scope); ?></h2>
+<div class="report-chart"></div>
 
-<?php echo do_shortcode("[case_data_chart xtitle='Care Provider Races' case_type='programmatic' data_set='provider_races' 3d='true' height='500px' filter_type='".$report_scope_slug."']"); ?>
-
-<div id="chartdiv"></div>
-
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-  // Ensure amCharts is loaded and ready
-  if (typeof am5 !== 'undefined') {
-    console.log("Initializing chart...");
-
-    // Create root element
-    var root = am5.Root.new("chartdiv");
-
-    // Set themes
-    root.setThemes([
-      am5themes_Animated.new(root)
-    ]);
-
-    // Create chart
-    var chart = root.container.children.push(am5percent.PieChart.new(root, {
-      layout: root.verticalLayout
-    }));
-
-    // Create series
-    var series = chart.series.push(am5percent.PieSeries.new(root, {
-      valueField: "value",
-      categoryField: "category"
-    }));
-
-    // Set data
-    series.data.setAll([
-      { value: 10, category: "One" },
-      { value: 9, category: "Two" },
-      { value: 6, category: "Three" },
-      { value: 5, category: "Four" },
-      { value: 4, category: "Five" },
-      { value: 3, category: "Six" },
-      { value: 1, category: "Seven" },
-    ]);
-
-    // Play initial series animation
-    series.appear(1000, 100);
-  } else {
-    console.error("amCharts library is not loaded.");
-  }
-});
-</script>
+<?php //echo do_shortcode("[case_data_chart xtitle='Care Provider Races' case_type='programmatic' data_set='provider_races' 3d='true' height='500px' filter_type='".$report_scope_slug."']"); ?>
 
 <?php
 // Capture buffered content in $test_content
-$test_content = ob_get_clean();
+$report_content = ob_get_clean();
 
 // Output or manipulate $test_content as needed
-echo $test_content;
+echo $report_content;
